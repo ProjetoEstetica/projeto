8931bb02ba40a7a038731d664e80eb1eb4cabcf1
diegobasilio <diegobasilio10@gmail.com>
2023-05-15 17:47:46 -0300

    DB Profissionais

--- db_profissionais.sql
+++ db_profissionais.sql
@@ -6,11 +6,8 @@ create table cadastro_profissionais(
 	nome_completo varchar(300),
     apelido varchar(100),
 	celular char(13),
-	aniversario date # insert into campo set evento = str_to_date ('31/05/2023', '%d%m%a')
-);
-
-create table endereco_profissionais(
-	cep varchar(50),
+	aniversario date, # insert into campo set evento = str_to_date ('31/05/2023', '%d%m%a')
+    cep varchar(50),
 	rua varchar(300),
 	numero int,
 	comp varchar(100),
@@ -18,5 +15,6 @@ create table endereco_profissionais(
 	cidade varchar(100),
 	estado char(3)
 );
+
 
 

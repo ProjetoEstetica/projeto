4f594768becf23be6e706fed42c88acce17039ff
diegobasilio <diegobasilio10@gmail.com>
2023-05-15 16:05:46 -0300

    Database Profissionais

--- db_profissionais.sql
+++ db_profissionais.sql
@@ -2,6 +2,7 @@ create database profissionais;
 use profissionais;
 
 create table cadastro_profissionais(
+	id int not null auto_increment primary key,
 	nome_completo varchar(300),
     apelido varchar(100),
 	celular char(13),
@@ -9,6 +10,7 @@ create table cadastro_profissionais(
 );
 
 create table endereco_profissionais(
+	id int not null auto_increment primary key,
 	cep varchar(50),
 	rua varchar(300),
 	numero int,

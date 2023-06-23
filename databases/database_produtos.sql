81f7ea9c0e91d092240634744d4e394ce8696a4b
EduBatageli <edumbat@gmail.com>
2023-06-23 10:15:56 -0300

    organizando

---
+++ databases/database_produtos.sql
@@ -0,0 +1,7 @@
+create database produtos;
+
+use produtos;
+
+create table cadastro_produtos(
+produto varchar(350), tipo_produto varchar(150), custo double, proco_venda double, desconto_max double, desconto_promo double, comissao int, desconto_comi varchar(4));
+
